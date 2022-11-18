c130ca55b7231a70e1cf627b502b6a2e26f5abe0
Ethan <ethan84hill@gmail.com>
2022-11-18 17:38:29 -0500

    lab almost done

---
+++ models/Room.model.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+var User = require('../models/User.model');
+
+const Schema = mongoose.Schema;
+
+const roomSchema = new Schema({
+    name: { type: String },
+    description: { type: String },
+    imageUrl: { type: String },
+    owner: { type: Schema.Types.ObjectId, ref: "User" },
+    reviews: [] // we will update this field a bit later when we create review model
+  });
+
+const Room = mongoose.model('Room', roomSchema);
+
+module.exports = Room;
---
+++ routes/index.js
@@ -0,0 +1,123 @@
+var express = require('express');
+var router = express.Router();
+const bcryptjs = require('bcryptjs');
+var User = require('../models/User.model');
+const Room = require('../models/Room.model');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/signup', (req, res, next) => {
+  res.render('signup.hbs')
+})
+
+router.post('/signup', (req, res, next) => {
+  console.log(req.body)
+
+  if(!req.body.email || !req.body.password) {
+      res.send('sorry you forgot an email or password')
+      return;
+  }
+
+  User.findOne({ email: req.body.email })
+  .then(foundUser => {
+      if(foundUser) {
+          res.send('sorry user already exists')
+          return;
+      }
+
+      return User.create({
+          email: req.body.email,
+          password: bcryptjs.hashSync(req.body.password),
+          fullName: req.body.fullName
+      })
+  })
+
+  .then(createdUser => {
+      console.log("here's the new user", createdUser)
+      res.redirect('/')
+  })
+  .catch(err => {
+      console.log(err)
+      res.send(err)
+  })
+})
+
+router.get('/login', (req, res, next) => {
+  res.render('login.hbs')
+})
+
+
+
+router.post('/login', (req, res, next) => {
+
+  const { email, password } = req.body
+
+  if(!email || !password) {
+      res.render('login.hbs', { errorMessage: 'sorry you forgot email or password'})
+      return;
+  }
+
+  User.findOne({ email: email })
+      .then(foundUser => {
+
+          if(!foundUser) {
+              // res.send('sorry user does not exist')
+              res.render('login.hbs', { errorMessage: 'sorry user does not exist' })
+              return;
+          }
+
+          const isValidPassword = bcryptjs.compareSync(password, foundUser.password)
+
+          if(!isValidPassword) {
+              // res.send('sorry wrong password')
+              res.render('login.hbs', { errorMessage: 'sorry wrong password' })
+              return;
+          }
+
+          req.session.user = foundUser
+          // res.send('logged in')
+          res.render('index.hbs', foundUser)
+      })
+      .catch(err => {
+          console.log(err)
+          res.send(err)
+      })
+})
+
+router.get('/logout', (req, res, next) => {
+  req.session.destroy(() => {
+      res.redirect('/');
+  })
+})
+
+
+router.get("/room/create", (req, res, next) => {
+  res.render('create-room')
+});
+
+router.post('/room/create', (req, res, next) => {
+  console.log(req.body)
+  Room.create({
+      name: req.body.name,
+      description: req.body.description,
+      imageUrl: req.body.imageUrl,
+      owner: req.session.user
+  })
+      .then(createdRoom => {
+          console.log('i created a room', createdRoom);
+          res.redirect('/')
+      })
+      .catch(err => {
+          res.send(err)
+  })
+})
+
+
+router.get('/list-rooms', (req, res, next) => {
+  res.render('list-rooms.hbs', {name: name})
+})
+
+module.exports = router;
